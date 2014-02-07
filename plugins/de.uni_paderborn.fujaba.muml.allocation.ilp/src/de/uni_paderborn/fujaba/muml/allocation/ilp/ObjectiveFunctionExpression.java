69d847d61e328b197e6a0f02a6b8d8b1de9594f0
Marcus Huewe <suse-tux@gmx.de>
2014-02-07 14:40:04 +0000

    - initial version of ILP meta model (created together with Andreas)

---
+++ plugins/de.uni_paderborn.fujaba.muml.allocation.ilp/src/de/uni_paderborn/fujaba/muml/allocation/ilp/ObjectiveFunctionExpression.java
@@ -0,0 +1,57 @@
+/**
+ */
+package de.uni_paderborn.fujaba.muml.allocation.ilp;
+
+import org.storydriven.core.expressions.Expression;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Objective Function Expression</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * This class represents an Integer Linear Program's objective function.
+ * <!-- end-model-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveFunctionExpression#getGoal <em>Goal</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see de.uni_paderborn.fujaba.muml.allocation.ilp.IlpPackage#getObjectiveFunctionExpression()
+ * @model
+ * @generated
+ */
+public interface ObjectiveFunctionExpression extends Expression {
+	/**
+	 * Returns the value of the '<em><b>Goal</b></em>' attribute.
+	 * The literals are from the enumeration {@link de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveGoal}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Goal</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Goal</em>' attribute.
+	 * @see de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveGoal
+	 * @see #setGoal(ObjectiveGoal)
+	 * @see de.uni_paderborn.fujaba.muml.allocation.ilp.IlpPackage#getObjectiveFunctionExpression_Goal()
+	 * @model required="true"
+	 * @generated
+	 */
+	ObjectiveGoal getGoal();
+
+	/**
+	 * Sets the value of the '{@link de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveFunctionExpression#getGoal <em>Goal</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Goal</em>' attribute.
+	 * @see de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveGoal
+	 * @see #getGoal()
+	 * @generated
+	 */
+	void setGoal(ObjectiveGoal value);
+
+} // ObjectiveFunctionExpression
---
+++ plugins/de.uni_paderborn.fujaba.muml.allocation.ilp/src/de/uni_paderborn/fujaba/muml/allocation/ilp/impl/ObjectiveFunctionExpressionImpl.java
@@ -0,0 +1,165 @@
+/**
+ */
+package de.uni_paderborn.fujaba.muml.allocation.ilp.impl;
+
+import de.uni_paderborn.fujaba.muml.allocation.ilp.IlpPackage;
+import de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveFunctionExpression;
+import de.uni_paderborn.fujaba.muml.allocation.ilp.ObjectiveGoal;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+import org.storydriven.core.expressions.impl.ExpressionImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Objective Function Expression</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link de.uni_paderborn.fujaba.muml.allocation.ilp.impl.ObjectiveFunctionExpressionImpl#getGoal <em>Goal</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ObjectiveFunctionExpressionImpl extends ExpressionImpl implements ObjectiveFunctionExpression {
+	/**
+	 * The default value of the '{@link #getGoal() <em>Goal</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getGoal()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final ObjectiveGoal GOAL_EDEFAULT = ObjectiveGoal.MIN;
+
+	/**
+	 * The cached value of the '{@link #getGoal() <em>Goal</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getGoal()
+	 * @generated
+	 * @ordered
+	 */
+	protected ObjectiveGoal goal = GOAL_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ObjectiveFunctionExpressionImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return IlpPackage.Literals.OBJECTIVE_FUNCTION_EXPRESSION;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ObjectiveGoal getGoal() {
+		return goal;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setGoal(ObjectiveGoal newGoal) {
+		ObjectiveGoal oldGoal = goal;
+		goal = newGoal == null ? GOAL_EDEFAULT : newGoal;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, IlpPackage.OBJECTIVE_FUNCTION_EXPRESSION__GOAL, oldGoal, goal));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case IlpPackage.OBJECTIVE_FUNCTION_EXPRESSION__GOAL:
+				return getGoal();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case IlpPackage.OBJECTIVE_FUNCTION_EXPRESSION__GOAL:
+				setGoal((ObjectiveGoal)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case IlpPackage.OBJECTIVE_FUNCTION_EXPRESSION__GOAL:
+				setGoal(GOAL_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case IlpPackage.OBJECTIVE_FUNCTION_EXPRESSION__GOAL:
+				return goal != GOAL_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (goal: ");
+		result.append(goal);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ObjectiveFunctionExpressionImpl
