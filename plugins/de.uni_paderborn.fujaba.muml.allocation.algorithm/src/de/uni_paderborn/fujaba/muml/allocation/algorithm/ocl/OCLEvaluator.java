05b59c7b46e65d0d6c74d222bb965d08aed1e8dc
Marcus Huewe <suse-tux@gmx.de>
2014-06-19 18:35:18 +0000

    - do not restrict OCLEvaluator API to EObject (the more general Object also works)

--- plugins/de.uni_paderborn.fujaba.muml.allocation.algorithm/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ocl/OCLEvaluator.java
+++ plugins/de.uni_paderborn.fujaba.muml.allocation.algorithm/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/ocl/OCLEvaluator.java
@@ -1,6 +1,5 @@
 package de.uni_paderborn.fujaba.muml.allocation.algorithm.ocl;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.examples.domain.evaluation.DomainModelManager;
 import org.eclipse.ocl.examples.pivot.Element;
@@ -13,12 +12,11 @@ import org.eclipse.ocl.examples.pivot.utilities.PivotEnvironmentFactory;
 import org.eclipse.ocl.examples.xtext.essentialocl.essentialoclcs.ContextCS;
 
 public class OCLEvaluator {
-	
 	/**
 	 * Evaluates the given OCL expression in the context of
 	 * the specified contextObject.
 	 */
-	public static Object evaluate(@NonNull ContextCS csExpression, @NonNull EObject contextObject) {
+	public static Object evaluate(@NonNull ContextCS csExpression, @NonNull Object contextObject) {
 		Element element = csExpression.getPivot();
 		if (!(element instanceof ExpressionInOCL)) {
 			throw new IllegalArgumentException("expected ExpressionInOCL instance; got: " + element);
@@ -30,17 +28,16 @@ public class OCLEvaluator {
 	 * Evaluates the given OCL expression in the context of
 	 * the specified contextObject.
 	 */
-	public static Object evaluate(@NonNull ExpressionInOCL expressionInOCL, @NonNull EObject contextObject) {
+	public static Object evaluate(@NonNull ExpressionInOCL expressionInOCL, @NonNull Object contextObject) {
 		// code is taken from org.eclipse.ocl.examples.xtext.console.OCLConsolePage
 		MetaModelManager metaModelManager = getMetaModelManager();
 		PivotEnvironmentFactory factory = new PivotEnvironmentFactory(null,
 				metaModelManager);
 		PivotEnvironment environment = factory.createEnvironment();
 		EvaluationEnvironment evaluationEnvironment = factory.createEvaluationEnvironment();
-		// contextObject is already a boxed object, thus there is no
-		// need to compute the boxed value
+		Object contextValue = metaModelManager.getIdResolver().boxedValueOf(contextObject);
 		evaluationEnvironment.add(expressionInOCL.getContextVariable(),
-				contextObject);
+				contextValue);
 		DomainModelManager modelManager = evaluationEnvironment.createModelManager(contextObject);
 		return new EvaluationVisitorImpl(environment, evaluationEnvironment, modelManager).visit(expressionInOCL);
 	}
