71cd28a4f8e0447af2bb605b6de07fed3a0730cc
Marcus Huewe <suse-tux@gmx.de>
2014-07-23 09:52:13 +0000

    - propagate ret code of the ilp solver down to the AllocationHelper lib

--- plugins/de.uni_paderborn.fujaba.muml.allocation.algorithm/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/lpsolve/LPSolve.java
+++ plugins/de.uni_paderborn.fujaba.muml.allocation.algorithm/src/de/uni_paderborn/fujaba/muml/allocation/algorithm/lpsolve/LPSolve.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
 import org.eclipse.m2m.qvt.oml.util.Dictionary;
-import org.eclipse.m2m.qvt.oml.util.Utils;
 
 import de.uni_paderborn.fujaba.muml.allocation.ilp.IntegerLinearProgram;
 import de.uni_paderborn.fujaba.muml.allocation.ilp.lpsolve.xtext.resource.LPSolveResource;
@@ -23,12 +22,13 @@ public class LPSolve {
 	}
 	
 	@Operation(kind=Kind.QUERY)
-	public static Dictionary<String, Integer> solve(IntegerLinearProgram ilp) {
-		return run(serialize(ilp));
+	public static int solve(IntegerLinearProgram ilp, Dictionary<String, Integer> solution) {
+		return run(serialize(ilp), solution);
 	}
 	
-	private static Dictionary<String, Integer> run(String ilpString) {
+	private static int run(String ilpString, Dictionary<String, Integer> solution) {
 		ProcessBuilder pb = new ProcessBuilder(CMD);
+		int ret = -1;
 		try {
 			/*FileWriter fw = new FileWriter("/home/marcus/run.lp");
 			fw.write(ilpString);
@@ -36,28 +36,27 @@ public class LPSolve {
 			Process process = pb.start();
 			process.getOutputStream().write(ilpString.getBytes());
 			process.getOutputStream().close();
-			int res = process.waitFor();
-			System.out.println("lp_solve: " + res);
-			if (res == 0) {
-				return parseOutput(process.getInputStream());
+			ret = process.waitFor();
+			System.out.println("lp_solve: " + ret);
+			if (ret == 0) {
+				parseOutput(process.getInputStream(), solution);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return ret;
 	}
 	
-	private static Dictionary<String, Integer> parseOutput(InputStream in) {
-		Dictionary<String, Integer> dict = Utils.<String, Integer>createDictionary();
+	private static void parseOutput(InputStream in, Dictionary<String, Integer> solution) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		try {
 			boolean variableSection = false;
 			String line = reader.readLine();
 			while (line != null) {
 				if (variableSection) {
-					parseLine(line, dict);
+					parseLine(line, solution);
 				}
 				if (line.equals(VAR_SECTION)) {
 					variableSection = true;
@@ -67,7 +66,6 @@ public class LPSolve {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return dict;
 	}
 	
 	private static void parseLine(String line,  Dictionary<String, Integer> map) {
