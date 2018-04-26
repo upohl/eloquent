e7118080f49b1e2c5bcf93dd6d021335b9664edd
Marcus Huewe <suse-tux@gmx.de>
2018-04-26 15:34:19 +0200

    Make sure the *.lp file extension is registered (testsuite)

--- tests/org.muml.psm.allocation.algorithm.ilp.lpsolve.tests/src/org/muml/psm/allocation/algorithm/ilp/lpsolve/tests/AllTestsSuite.java
+++ tests/org.muml.psm.allocation.algorithm.ilp.lpsolve.tests/src/org/muml/psm/allocation/algorithm/ilp/lpsolve/tests/AllTestsSuite.java
@@ -1,13 +1,18 @@
 package org.muml.psm.allocation.algorithm.ilp.lpsolve.tests;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import org.muml.psm.allocation.ilp.lpsolve.xtext.LPSolveStandaloneSetup;
 
 @RunWith(Suite.class)
 @SuiteClasses({
 	LPSolveM2TTest.class
 })
 public class AllTestsSuite {
-
+	@BeforeClass
+	public static void setup() {
+		LPSolveStandaloneSetup.doSetup();
+	}
 }
