d100287b40e5aec9513265b4d898e3921de20af1
Marcus Huewe <suse-tux@gmx.de>
2018-05-22 20:18:10 +0200

    Refactor OpenAllocationWizardHandler into AbstractOpenWizardHandler
    So that the generic opening code can be used to open the
    AllocationComputationWizard with different PageContexts.

--- plugins/org.muml.psm.allocation.algorithm.ui/src/org/muml/psm/allocation/algorithm/ui/OpenAllocationWizardHandler.java
+++ plugins/org.muml.psm.allocation.algorithm.ui/src/org/muml/psm/allocation/algorithm/ui/OpenAllocationWizardHandler.java
@@ -1,34 +1,12 @@
 package org.muml.psm.allocation.algorithm.ui;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.handlers.HandlerUtil;
-import org.muml.psm.allocation.algorithm.ui.wizard.AbstractAllocationWizard;
-import org.muml.psm.allocation.algorithm.ui.wizard.AllocationComputationWizard;
 import org.muml.psm.allocation.algorithm.ui.wizard.PageContext;
 
-public class OpenAllocationWizardHandler extends AbstractHandler {
+public class OpenAllocationWizardHandler extends AbstractOpenWizardHandler {
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbench workbench = HandlerUtil.getActiveWorkbenchWindow(event)
-				.getWorkbench();
-		Shell shell = HandlerUtil.getActiveShell(event);
-		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		IStructuredSelection structuredSelection = selection instanceof IStructuredSelection ? (IStructuredSelection) selection
-				: null;
-		//IWorkbenchWizard allocationWizard = new AllocationWizard();
-		AbstractAllocationWizard allocationWizard = new AllocationComputationWizard(PageContext.AllocationComputation);
-		allocationWizard.init(workbench, structuredSelection);
-		WizardDialog wizardDialog = new WizardDialog(shell, allocationWizard);
-		wizardDialog.open();
-		return null;
+	protected PageContext getPageContext() {
+		return PageContext.AllocationComputation;
 	}
 
 }
