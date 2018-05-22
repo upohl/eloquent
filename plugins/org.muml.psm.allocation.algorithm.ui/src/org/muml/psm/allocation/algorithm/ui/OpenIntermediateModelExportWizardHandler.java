77a619f7051be2d5a9fd60a95e4c0b610535dc89
Marcus Huewe <suse-tux@gmx.de>
2018-05-22 20:36:44 +0200

    Register "Intermediate Model Export" popup menu

---
+++ plugins/org.muml.psm.allocation.algorithm.ui/src/org/muml/psm/allocation/algorithm/ui/OpenIntermediateModelExportWizardHandler.java
@@ -0,0 +1,12 @@
+package org.muml.psm.allocation.algorithm.ui;
+
+import org.muml.psm.allocation.algorithm.ui.wizard.PageContext;
+
+public class OpenIntermediateModelExportWizardHandler extends AbstractOpenWizardHandler {
+
+	@Override
+	protected PageContext getPageContext() {
+		return PageContext.IntermediateModelExport;
+	}
+
+}
