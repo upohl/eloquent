upohl/eloquent

commit af6a9a0ca84bb72de90813048e626fcd04cafae8

Consider 0..1 references in AbstractQVToBasedMultiConfigurationStrategy
Until now, we always assumed 1..1.

plugins/org.muml.psm.allocation.algorithm/src/org/muml/psm/allocation/algorithm/qvto/AbstractQVToBasedMultiConfigurationStrategy.java
@@ -57,8 +57,11 @@ public abstract class AbstractQVToBasedMultiConfigurationStrategy implements ISt
 				throw new IllegalStateException(
 						String.format(manyFeatureError, reference));
 			}
-			// hmm one big "dict"?
-			buildDictionaryString(builder, (EObject) eObject.eGet(reference));
+			eObject = (EObject) eObject.eGet(reference);
+			if (eObject != null) {
+				// hmm one big "dict"?
+				buildDictionaryString(builder, (EObject) eObject.eGet(reference));
+			}
 		}
 	}
 	

commit 07785d86108b0444d0e80796e48fd08bb20790a0

Fix possible NPE
This bug was introduced in commit
af6a9a0ca84bb72de90813048e626fcd04cafae8 ("Consider 0..1 references
in AbstractQVToBasedMultiConfigurationStrategy").

plugins/org.muml.psm.allocation.algorithm/src/org/muml/psm/allocation/algorithm/qvto/AbstractQVToBasedMultiConfigurationStrategy.java
@@ -60,7 +60,7 @@ public abstract class AbstractQVToBasedMultiConfigurationStrategy implements ISt
 			eObject = (EObject) eObject.eGet(reference);
 			if (eObject != null) {
 				// hmm one big "dict"?
-				buildDictionaryString(builder, (EObject) eObject.eGet(reference));
+				buildDictionaryString(builder, (EObject) eObject);
 			}
 		}
 	}